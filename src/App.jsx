c963db10778e89beb43ce6b23c2dab7a6cb1c0ef
kaushal8582 <kaushakumar02918@gmail.com>
2025-03-29 14:38:26 +0530

    style form

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,41 @@
+import { useContext, useEffect, useState } from "react";
+import Form from "./components/Form";
+import Header from "./components/Header";
+import ShowImg from "./components/ShowImg";
 
-import { useContext, useEffect, useState } from 'react'
-import Form from './components/Form'
-import Header from './components/Header'
-import ShowImg from './components/ShowImg'
-
-import ReactDom from "react-dom"
-import blogContext from './store/blog-context'
+import ReactDom from "react-dom";
+import blogContext from "./store/blog-context";
 
 function App() {
+  const { isEdit } = useContext(blogContext);
 
-  const {isEdit} = useContext(blogContext)
+  const [open, setOpen] = useState(false);
 
-  const [open,setOpen] = useState(false)
-
-  function handleCloseClick(){
-    setOpen(false)
+  function handleCloseClick() {
+    setOpen(false);
   }
 
-  function handleOpenClick(){
-    setOpen(true)
+  function handleOpenClick() {
+    setOpen(true);
   }
 
-  useEffect(()=>{
-    if(isEdit){
-      setOpen(true)
+  useEffect(() => {
+    if (isEdit) {
+      setOpen(true);
     }
-  },[isEdit])
-
+  }, [isEdit]);
 
   return (
     <>
       <Header onOpen={handleOpenClick} />
-      <ShowImg/>
-      {open && ReactDom.createPortal(<Form onClose = {handleCloseClick} />,document.getElementById("root"))}
+      <ShowImg />
+      {open &&
+        ReactDom.createPortal(
+          <Form onClose={handleCloseClick} />,
+          document.getElementById("root")
+        )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
